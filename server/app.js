cb93d9ec48d7b98cd5de5d54d02aa8cfb16d435a
jjgesulgon <jj@hybrain.co>
2020-06-30 14:04:10 +0800

    setup graphql api

--- server/app.js
+++ server/app.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/api', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    //resolvers
+    rootValue: {
+        events: () => {
+            return ['Party', 'Studying', 'Sleeping'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.get('/', (req, res, next) => {
     res.send('Event Booking');
