fc326d211ab16a251e836546e2625e36dc2c800e
jjgesulgon <jj@hybrain.co>
2020-07-07 19:12:57 +0800

    remove unnecessary space

--- server/app.js
+++ server/app.js
@@ -4,17 +4,35 @@ const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const app = express();
+const events = [];
 
 app.use(bodyParser.json());
 
 app.use('/api', graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+            venue: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+            venue: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -22,14 +40,23 @@ app.use('/api', graphqlHttp({
             mutation: RootMutation
         }
     `),
+    
     //resolvers
     rootValue: {
         events: () => {
-            return ['Party', 'Studying', 'Sleeping'];
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date,
+                venue: args.eventInput.venue
+            }
+            events.push(event);
+            return event;
         }
     },
     graphiql: true
